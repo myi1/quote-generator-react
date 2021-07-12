357a31564a1e48f049e8db5fe414a308aa05488a
Muhammad Ismail <yahyaismail@gmail.com>
2021-07-12 03:20:32 -0400

    Rebuilt quote generator with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { Component } from "react";
+import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    quotesList: [],
+    quote: "",
+    author: "",
+    shouldUpdate: true,
+  };
+
+  componentDidMount() {
+    this.getQuotes();
+  }
+
+  componentDidUpdate() {
+    const { shouldUpdate } = this.state;
+    if (shouldUpdate) {
+      this.getNewQuote();
+    }
+  }
+
+  getQuotes() {
+    const apiURL = "https://type.fit/api/quotes";
+    axios
+      .get(apiURL)
+      .then((response) => {
+        this.setState({
+          ...this.state,
+          quotesList: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    this.getNewQuote();
+  }
+
+  getNewQuote = () => {
+    const quote =
+      this.state.quotesList[
+        Math.floor(Math.random() * this.state.quotesList.length)
+      ];
+    if (quote && !quote.author) {
+      console.log(quote);
+      this.setState({
+        ...this.state,
+        author: "Unknown",
+        quote: quote.text,
+        shouldUpdate: false,
+      });
+    } else if (quote) {
+      console.log(quote);
+      this.setState({
+        ...this.state,
+        author: quote.author,
+        quote: quote.text,
+        shouldUpdate: false,
+      });
+    }
+  };
+
+  tweetQuote = () => {
+    const { quote, author } = this.state;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, "_blank");
+  };
+
+  render() {
+    const { quote, author } = this.state;
+    if (!quote) {
+      return <div className='loader' id='loader'></div>;
+    }
+    return (
+      <div className='quote-container' id='quote-container'>
+        <div className='quote-text'>
+          <i className='fas fa-quote-left'></i>
+          <span id='quote' className={quote.length > 120 ? "long-quote" : null}>
+            {quote}
+          </span>
+
+          <div className='quote-author'>
+            <span id='author'>{author}</span>
+          </div>
+          <div className='button-container'>
+            <button className='button twitter' onClick={this.tweetQuote}>
+              <FontAwesomeIcon
+                icon={faTwitter}
+                id='twitter'
+                title='Tweet This!'
+              />
+            </button>
+
+            <button
+              id='new-quote'
+              className='button'
+              onClick={this.getNewQuote}>
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
